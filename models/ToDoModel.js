ee5aed7ae9049fceffa5286627a44a517486aded
abisheik88 <abishekvijayarangan@gmail.com>
2023-02-25 11:52:32 +0530

    Backend

--- models/ToDoModel.js
+++ models/ToDoModel.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const todoSchema = new mongoose.Schema({
     text: {
         type: String,
-        require: true
+        required: true,
+        minlength: 1
     }
 })
 
